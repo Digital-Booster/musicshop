ba2fc6828f9c1144334927ec1c84b732af79e462
Diego Leccardi <diego@digitalbooster.net>
2026-05-14 09:42:07 +0200

    added local folder

--- data/migrate.sh
+++ data/migrate.sh
@@ -1,5 +1,1 @@
 docker run --rm -v $(pwd):/data --network host dimitri/pgloader:latest pgloader  --debug /data/migrate.load
-
-
-# docker run --rm -v $(pwd):/data --network host dimitri/pgloader:latest \
-#   pgloader data/MusicShopSQLite.db postgresql://postgres:postgres@localhost:5432/musicshop
